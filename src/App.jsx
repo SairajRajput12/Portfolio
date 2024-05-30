6346c79c6e532ea352bdff7278d00bc94f990a3e
SairajRajput12 <125473711+SairajRajput12@users.noreply.github.com>
2024-05-30 11:58:17 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// src/App.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home'; 
+import Project from './components/Project'; 
+import About from './components/About'; 
+import Contact from './components/ContactUs'; 
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="project" element={<Project />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Navbar/Navbar.css'
+import Img from '../Navbar/file.png';
+export default function Navbar(){
+    return(
+        <div className='navbar'>
+            <div>
+                <img src={Img} alt='logo'></img>
+            </div>
+            <nav>
+                <Link className='navlink' to="/"><button>Home</button></Link>
+                <Link className='navlink' to="/about"><button>About</button></Link>
+                <Link className='navlink' to="/contact"><button>Contact</button></Link>
+                <Link className='navlink' to="/project"><button>Project</button></Link>
+            </nav>
+        </div>
+    );
+}
